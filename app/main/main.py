25db50c1d99b44812701b7c88e198446dddcd976
pete-dnae <pete.howard@dnae.com>
2017-10-01 23:24:43 +0100

    Added bootstrap libs and usage.

--- app/main/main.py
+++ app/main/main.py
@@ -1,10 +1,7 @@
-from flask import Flask
-
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return render_template('hello.html', name='hard Coded name')
+@app.route('/')
+def hello(name=None):
+    return render_template('hello.html', name='hard coded name')
