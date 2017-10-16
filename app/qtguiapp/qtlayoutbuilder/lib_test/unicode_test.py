475d3bf7c0d6c2c81b2ce53575c617d969f9419c
pete-dnae <pete.howard@dnae.com>
2017-10-16 20:44:10 +0100

    Added README for command line app.

--- app/qtguiapp/qtlayoutbuilder/lib_test/unicode_test.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-
-class TestUnicodeUtils(TestCase):
-
-    def test_playaround(self):
-        str_input = 'foo\u2127more'.decode('unicode_escape')
-        print 'v: <%s>, type %s' % (input, type(str_input))
