da5eaf64502a3aeeb0defc31da0cae2df1c0adac
pete-dnae <pete.howard@dnae.com>
2017-10-18 19:56:16 +0100

    Learned how to put in one jinja2 template referencing another with the include directive.

--- app/appengine/main.py
+++ app/appengine/main.py
@@ -1,37 +1,15 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# [START app]
 import logging
 
-# [START imports]
 from flask import Flask, render_template, request
-# [END imports]
 
-# [START create_app]
 app = Flask(__name__)
-# [END create_app]
+app.debug = True
 
-
-# [START form]
 @app.route('/main')
 def form():
     return render_template('main.html')
-# [END form]
 
-
-# [START submitted]
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
     name = request.form['name']
@@ -39,15 +17,12 @@ def submitted_form():
     site = request.form['site_url']
     comments = request.form['comments']
 
-    # [END submitted]
-    # [START render_template]
     return render_template(
         'submitted_form.html',
         name=name,
         email=email,
         site=site,
         comments=comments)
-    # [END render_template]
 
 
 @app.errorhandler(500)
@@ -55,4 +30,3 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-# [END app]
