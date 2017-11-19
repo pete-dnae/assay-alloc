289915a464a70b47d800869fa3421420f4959e5b
pete-dnae <pete.howard@dnae.com>
2017-11-19 23:40:22 +0000

    Finished refactoring, and fixed bug, where max permitted overlap should always be the constant value 2.

--- src/entrypoints/doe.py
+++ src/entrypoints/doe.py
@@ -4,16 +4,19 @@ different settings each time. Prints a success / fail line of output for each
 attempt made.
 """
 
-from model.experimentdesign import ExperimentDesign
+from lib.model.experimentdesign import ExperimentDesign
 
-from lib.model.allocators.deduction import DepletingPoolAllocator
+from lib.model.depletingpoolallocator import DepletingPoolAllocator
 
+all_targets = (3,4,5)
+all_assays = (13,16,18,20,22,25)
+all_chambers = (14,17,19,21,23,26)
 
 def run():
     print('TARGETS, ASSAYS, CHAMBERS, WORKED')
-    for targets in (3,4,5):
-        for assays in (13,16,18,20,22,25):
-            for chambers in (14,17,19,21,23,26):
+    for targets in all_targets:
+        for assays in all_assays:
+            for chambers in all_chambers:
                 experiment_design = ExperimentDesign.make_from_params(
                         assays, chambers, targets, 0)
                 allocator = DepletingPoolAllocator(experiment_design)
